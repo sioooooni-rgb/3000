e4b5656666fc4a2d71b577e7ea051d36c7d8e8c2
sioooooni-rgb <sioooooni0611@gmail.com>
2024-08-21 22:17:32 +0900

    third commit

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,6 +7,9 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
+  TextEditingController content_controller = TextEditingController();
+  TextEditingController position_controller = TextEditingController();
+
   int selected_index = 0;
   List<Widget> widget_options = [
     Text('home'),
@@ -23,24 +26,6 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(
-          "66",
-          style: TextStyle(
-            fontSize: 27.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            onPressed: (){
-
-            },
-            icon: Icon(Icons.supervisor_account_rounded),
-          )
-        ],
-      ),
       body: Builder(
         builder: (context) {
           if(selected_index == 0){
@@ -48,6 +33,24 @@ class MainPageState extends State<MainPage> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text(
+                    "66",
+                    style: TextStyle(
+                      fontSize: 27.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  actions: <Widget>[
+                    IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.supervisor_account_rounded),
+                    )
+                  ],
+                ),
                 Padding(
                   padding: EdgeInsets.all(40.0),
                   child: Row(
@@ -158,27 +161,159 @@ class MainPageState extends State<MainPage> {
             );
           }
           else if(selected_index == 1){
-            return Padding(
-              padding: EdgeInsets.all(40.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text(
+                    "${DateTime.now().month}/${DateTime.now().day} 묵상 기록",
+                    style: TextStyle(
+                      fontSize: 27.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: (){
 
-                ],
-              ),
+                      },
+                      child: Text(
+                        "기록",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(40.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: "읽은 구절",
+                              hintText: "2~10자리",
+                              filled: true,
+                              alignLabelWithHint: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            controller: position_controller,
+                          ),
+                        ),
+                        SizedBox(height: 20.0,),
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: "간단한 묵상 내용",
+                              hintText: "2~10자리",
+                              filled: true,
+                              alignLabelWithHint: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            controller: content_controller,
+                            maxLines: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             );
           }
           else if(selected_index == 2){
-            return Padding(
-              padding: EdgeInsets.all(40.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text(
+                    "이시온",
+                    style: TextStyle(
+                      fontSize: 27.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  actions: <Widget>[
+                    IconButton(
+                      onPressed: (){
 
-                ],
-              ),
+                      },
+                      icon: Icon(Icons.share),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0, bottom: 40.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 1,
+                      ),
+                      SizedBox(
+                        width: 70.0,
+                        height: 70.0,
+                        child: Container(
+                          child: SizedBox(),
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 1,
+                      ),
+                      Text(
+                        "통독\n0.5%",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 1,
+                      ),
+                      Text(
+                        "팔로워\n6명",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 1,
+                      ),
+                      Text(
+                        "팔로잉\n2명",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(),
+                        flex: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             );
           }
           else{
